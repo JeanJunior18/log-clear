8f10023f404a26b10f16a9c57a84e6460a79e46a
Jean Junior <jean.gomes@tatic.net>
2022-03-16 21:34:13 -0300

    feat: remove array of string match

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import fs from 'fs'
 
-const file = fs.readFileSync('./src/trigger/test.log', 'utf8')
-const lines = file
-  .split('\n')
-  .filter(line =>
-    !line
-      .toLocaleLowerCase()
-      .includes('report')
-  )
+const REMOVE_LINES_WITH = ['report', 'service']
+const ORIGIN_FILE_PATH = './src/trigger/test.log'
+const DESTINATION_FILE_PATH = './src/files/result.log'
 
-console.log(lines)
-fs.writeFileSync(`./src/files/${new Date().toISOString()}.log`, lines.join('\n'))
+function removeLinesWithWords(originFilePath, words, destinationFilePath) {
+  const file = fs.readFileSync(originFilePath, 'utf8')
+  const lines = file.split('\n').map(line => line.toLocaleLowerCase())
+  const newLines = lines.filter(line => !words.some(word => line.includes(word.toLocaleLowerCase())))
+  fs.writeFileSync(destinationFilePath, newLines.join('\n'))
+}
+
+removeLinesWithWords(ORIGIN_FILE_PATH, REMOVE_LINES_WITH, DESTINATION_FILE_PATH)
